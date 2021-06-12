f8edec388c4f71c3c124037cb35003f74e0fbb27
Steven <wgyngbh@gmail.com>
2021-06-12 13:49:03 +0800

    remove test

--- test/robot.test.js
+++ test/robot.test.js
@@ -1,27 +1,7 @@
-const path = require('path')
-const puppeteer = require('puppeteer')
-const { getPageName, getPageLinks } = require('../src/robot')
+const { getPageName } = require('../src/robot')
 const { expect } = require('chai')
 
 describe('robot.js', function () {
-  let browser
-  before(async function () {
-    browser = await puppeteer.launch({
-      executablePath: path.resolve(
-        __dirname,
-        '../chrome-mac/Chromium.app/Contents/MacOS/Chromium'
-      ),
-      timeout: 10000,
-      ignoreHTTPSErrors: true,
-      devtools: false,
-      headless: true
-    })
-  })
-
-  after(async function () {
-    await browser.close()
-  })
-
   it('getPageName返回index.html', function () {
     expect(
       getPageName('http://www.robotstxt.org/', 'http://www.robotstxt.org/')
@@ -35,15 +15,5 @@ describe('robot.js', function () {
         'http://www.robotstxt.org/'
       )
     ).equal('abc.html')
-  })
-
-  it('getPageLinks', async function () {
-    const links = await getPageLinks(browser, 'http://www.robotstxt.org/')
-    /* eslint-disable-next-line */
-    expect(links).to.not.be.null
-    /* eslint-disable-next-line */
-    expect(links).to.not.be.undefined
-    /* eslint-disable-next-line */
-    expect(links).to.not.be.empty
   })
 })
